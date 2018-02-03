a96bd21e60ea5e4a1ad68efd17a0a2f801555c88
linwenjun <linwenjun@163.com>
2018-02-03 21:17:34 +0800

    change config of kube

--- deployToDEV.sh
+++ deployToDEV.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 
 kubectl --kubeconfig=$KUBECTL_CONFIG_FILE delete -f inventory-service.yaml
-sleep 50
 kubectl --kubeconfig=$KUBECTL_CONFIG_FILE create -f inventory-service.yaml
