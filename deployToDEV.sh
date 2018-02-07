6c862c4536bce0fcf40029a0aac39e9f746c9cbf
linwenjun <linwenjun@163.com>
2018-02-08 01:50:04 +0800

    change deploy script

--- deployToDEV.sh
+++ deployToDEV.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 
+JENKINS_DOCKER_IMAGE=$DMALL_DOCKER_REGISTRY/$SLUG/inventory-service:$BUILD_NUMBER
+sed -i -e "s|image: .*|image: ${JENKINS_DOCKER_IMAGE}|g" inventory-service.yaml
+
 kubectl --kubeconfig=$KUBECTL_CONFIG_FILE apply -f inventory-service.yaml
