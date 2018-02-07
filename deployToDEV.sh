6283976a29916c7d9681628cb0cf7292094e276c
linwenjun <linwenjun@163.com>
2018-02-08 01:37:36 +0800

    change deploy to dev script

--- deployToDEV.sh
+++ deployToDEV.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
 
-kubectl --kubeconfig=$KUBECTL_CONFIG_FILE delete -f inventory-service.yaml
-kubectl --kubeconfig=$KUBECTL_CONFIG_FILE create -f inventory-service.yaml
+kubectl --kubeconfig=$KUBECTL_CONFIG_FILE apply -f inventory-service.yaml
